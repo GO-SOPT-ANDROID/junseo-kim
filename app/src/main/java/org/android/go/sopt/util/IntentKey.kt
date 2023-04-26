3aad5363fbb3b30ea0f9d0a636def2bac9b2e784
giovannijunseokim <kingjunseo@gmail.com>
2023-04-26 17:05:39 +0900

    [feat] Intent Key 상수로 관리

---
+++ app/src/main/java/org/android/go/sopt/util/IntentKey.kt
@@ -0,0 +1,8 @@
+package org.android.go.sopt.util
+
+object IntentKey {
+    const val USER_ID = "USER_ID"
+    const val USER_PW = "USER_PW"
+    const val USER_NAME = "USER_NAME"
+    const val USER_SKILL = "USER_SKILL"
+}
