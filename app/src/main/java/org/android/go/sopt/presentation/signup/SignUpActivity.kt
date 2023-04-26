4ad13146d8e3644aa8be9f9a8103bad5748149df
giovannijunseokim <kingjunseo@gmail.com>
2023-04-26 17:05:48 +0900

    [feat] SignUpActivity 로그인 기능 구현

--- app/src/main/java/org/android/go/sopt/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignUpActivity.kt
@@ -1,8 +1,14 @@
 package org.android.go.sopt.presentation.signup
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.android.go.sopt.databinding.ActivitySignUpBinding
+import org.android.go.sopt.presentation.signin.view.SignInActivity
+import org.android.go.sopt.util.IntentKey.USER_ID
+import org.android.go.sopt.util.IntentKey.USER_NAME
+import org.android.go.sopt.util.IntentKey.USER_PW
+import org.android.go.sopt.util.IntentKey.USER_SKILL
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -10,5 +16,23 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSignUpBtnClickListener()
+    }
+
+    private fun setSignUpBtnClickListener() {
+        binding.btnSignUp.setOnClickListener {
+            completeSignUp()
+        }
+    }
+
+    private fun completeSignUp() {
+        setResult(RESULT_OK, Intent(this, SignInActivity::class.java).apply {
+            putExtra(USER_ID, binding.etSignUpId.text.toString())
+            putExtra(USER_PW, binding.etSignUpPw.text.toString())
+            putExtra(USER_NAME, binding.etSignUpName.text.toString())
+            putExtra(USER_SKILL, binding.etSignUpSkill.text.toString())
+        })
+        if (!isFinishing) finish()
     }
 }
