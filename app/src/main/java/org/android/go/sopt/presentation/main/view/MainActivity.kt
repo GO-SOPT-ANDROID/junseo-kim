61971f7a6fabcb062c0eb0ed279487303f72eb50
giovannijunseokim <kingjunseo@gmail.com>
2023-04-26 20:06:31 +0900

    [feat] 화면에 유저 정보 가져오는 기능 구현

--- app/src/main/java/org/android/go/sopt/presentation/main/view/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/main/view/MainActivity.kt
@@ -3,10 +3,28 @@ package org.android.go.sopt.presentation.main.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.android.go.sopt.R
+import org.android.go.sopt.databinding.ActivityMainBinding
+import org.android.go.sopt.util.IntentKey.USER_NAME
+import org.android.go.sopt.util.IntentKey.USER_SKILL
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setUserInfoOnScreen()
+
+        setContentView(binding.root)
+    }
+
+    private fun setUserInfoOnScreen() {
+        with(binding) {
+            ivMainUser.setImageResource(R.drawable.img_user_profile)
+            tvMainUserName.text = getString(R.string.user_name_is, intent.getStringExtra(USER_NAME))
+            tvMainUserSkill.text = getString(
+                R.string.user_skill_is, intent.getStringExtra(
+                    USER_SKILL
+                )
+            )
+        }
     }
 }
