94fc310ac1549e0afac4371a940b14660bf34fc9
giovannijunseokim <kingjunseo@gmail.com>
2023-04-26 19:13:57 +0900

    [feat] 유저 정보 data class 추가

---
+++ app/src/main/java/org/android/go/sopt/model/UserInfo.kt
@@ -0,0 +1,8 @@
+package org.android.go.sopt.model
+
+data class UserInfo(
+    var userId: String? = null,
+    var userPw: String? = null,
+    var userName: String? = null,
+    var userSkill: String? = null,
+)
