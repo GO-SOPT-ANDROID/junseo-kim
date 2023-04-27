b54d920e5dd4f10bb393dd7687aaddff76dd51c6
giovannijunseokim <kingjunseo@gmail.com>
2023-04-27 15:16:49 +0900

    [feat] Encrypted Shared Preferences 객체 구현

---
+++ app/src/main/java/org/android/go/sopt/data/local/SharedPreferences.kt
@@ -0,0 +1,52 @@
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import org.android.go.sopt.R
+import org.android.go.sopt.util.PublicString.BLANK
+
+object SharedPreferences {
+    private lateinit var preferences: SharedPreferences
+
+    fun init(context: Context) {
+        val masterKeyAlias = MasterKey
+            .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        preferences = EncryptedSharedPreferences.create(
+            context,
+            context.getString(R.string.shared_preferences_key),
+            masterKeyAlias,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    fun setString(key: String, value: String?) {
+        preferences.edit {
+            putString(key, value)
+        }
+    }
+
+    fun getString(key: String): String? {
+        return preferences.getString(key, BLANK)
+    }
+
+    fun setBoolean(key: String, boolean: Boolean) {
+        preferences.edit {
+            putBoolean(key, boolean)
+        }
+    }
+
+    fun getBoolean(key: String): Boolean {
+        return preferences.getBoolean(key, false)
+    }
+
+    fun clear() {
+        preferences.edit {
+            clear()
+        }
+    }
+}
