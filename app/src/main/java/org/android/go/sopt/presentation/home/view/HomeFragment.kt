6ee8ebd92d1a2424c1de54548e708d9dfd8bdef6
giovannijunseokim <kingjunseo@gmail.com>
2023-05-03 01:20:35 +0900

    [feat] ListAdapter를 통한 리사이클러 뷰 구현

--- app/src/main/java/org/android/go/sopt/presentation/home/view/HomeFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/view/HomeFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import org.android.go.sopt.databinding.FragmentHomeBinding
+import org.android.go.sopt.presentation.home.adapter.GoAndroidAdapter
+import org.android.go.sopt.presentation.home.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { "binding is null ...." }
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +28,13 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
 
+    private fun initRecyclerView() {
+        val adapter = GoAndroidAdapter()
+        binding.rvHome.adapter = adapter
+        adapter.submitList(viewModel.getPartMemberList())
     }
 
     override fun onDestroyView() {
