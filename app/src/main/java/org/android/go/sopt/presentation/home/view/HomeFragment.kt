0d126ec9a5661f1f0c1b1470c6fa2ecf18e5e1e7
giovannijunseokim <kingjunseo@gmail.com>
2023-05-04 03:21:07 +0900

    [feat] adapter에 SelectionTracker 전달

--- app/src/main/java/org/android/go/sopt/presentation/home/view/HomeFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/view/HomeFragment.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
+import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentHomeBinding
 import org.android.go.sopt.presentation.home.adapter.GoAndroidAdapter
+import org.android.go.sopt.presentation.home.adapter.selection.GoAndroidItemDetailsLookup
+import org.android.go.sopt.presentation.home.adapter.selection.SelectionItemKeyProvider
 import org.android.go.sopt.presentation.home.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
@@ -28,6 +33,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         initRecyclerView()
     }
 
@@ -35,7 +41,17 @@ class HomeFragment : Fragment() {
         val adapter = GoAndroidAdapter()
         binding.rvHome.adapter = adapter
         adapter.submitList(viewModel.getPartMemberList())
+        val tracker = buildTracker()
+        adapter.setSelectionTracker(tracker)
     }
+
+    private fun buildTracker(): SelectionTracker<Long> = SelectionTracker.Builder(
+        getString(R.string.selected_member),
+        binding.rvHome,
+        SelectionItemKeyProvider(binding.rvHome),
+        GoAndroidItemDetailsLookup(binding.rvHome),
+        StorageStrategy.createLongStorage(),
+    ).build()
 
     fun smoothScrollToTop() {
         binding.rvHome.smoothScrollToPosition(0)
