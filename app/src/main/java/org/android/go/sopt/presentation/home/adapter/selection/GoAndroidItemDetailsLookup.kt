3608b92f21d754715c13fa58084673921df3e12b
giovannijunseokim <kingjunseo@gmail.com>
2023-05-04 03:20:27 +0900

    [feat] GoAndroidItemDetailsLookup 생성

---
+++ app/src/main/java/org/android/go/sopt/presentation/home/adapter/selection/GoAndroidItemDetailsLookup.kt
@@ -0,0 +1,20 @@
+package org.android.go.sopt.presentation.home.adapter.selection
+
+import android.view.MotionEvent
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.widget.RecyclerView
+import org.android.go.sopt.presentation.home.adapter.GoAndroidAdapter
+
+class GoAndroidItemDetailsLookup(private val recyclerView: RecyclerView) :
+    ItemDetailsLookup<Long>() {
+    override fun getItemDetails(e: MotionEvent): ItemDetails<Long>? {
+        val view = recyclerView.findChildViewUnder(e.x, e.y) ?: return null
+        val viewHolder = recyclerView.getChildViewHolder(view)
+        return if (viewHolder.itemViewType == GoAndroidAdapter.HEADER) {
+            (viewHolder as GoAndroidAdapter.HeaderViewHolder).getItemDetails()
+        } else {
+            (viewHolder as GoAndroidAdapter.GoAndroidViewHolder).getItemDetails()
+        }
+    }
+
+}
