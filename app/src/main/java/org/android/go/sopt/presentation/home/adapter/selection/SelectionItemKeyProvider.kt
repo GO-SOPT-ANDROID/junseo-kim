7711fc41014cba6fce95fcde3add6ed036e42942
giovannijunseokim <kingjunseo@gmail.com>
2023-05-04 03:20:16 +0900

    [feat] SelectionItemKeyProvider 생성

---
+++ app/src/main/java/org/android/go/sopt/presentation/home/adapter/selection/SelectionItemKeyProvider.kt
@@ -0,0 +1,18 @@
+package org.android.go.sopt.presentation.home.adapter.selection
+
+import androidx.recyclerview.selection.ItemKeyProvider
+import androidx.recyclerview.widget.RecyclerView
+
+class SelectionItemKeyProvider(
+    private val recyclerView: RecyclerView,
+) : ItemKeyProvider<Long>(SCOPE_MAPPED) {
+    override fun getKey(position: Int): Long {
+        val holder = recyclerView.findViewHolderForAdapterPosition(position)
+        return holder?.itemId ?: RecyclerView.NO_ID
+    }
+
+    override fun getPosition(key: Long): Int {
+        val holder = recyclerView.findViewHolderForItemId(key)
+        return holder?.adapterPosition ?: RecyclerView.NO_POSITION
+    }
+}
