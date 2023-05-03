62f732031736b9de4c500ae68baf7791cf0b6e83
giovannijunseokim <kingjunseo@gmail.com>
2023-05-03 14:59:49 +0900

    [feat] Multi-View Type RecyclerView 구현

--- app/src/main/java/org/android/go/sopt/presentation/home/adapter/GoAndroidAdapter.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/adapter/GoAndroidAdapter.kt
@@ -7,27 +7,54 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.android.go.sopt.data.model.PartMember
 import org.android.go.sopt.databinding.ItemGoAndroidBinding
+import org.android.go.sopt.databinding.ItemGoAndroidHeaderBinding
 
-class GoAndroidAdapter : ListAdapter<PartMember, GoAndroidAdapter.GoAndroidViewHolder>(diffUtil) {
+class GoAndroidAdapter : ListAdapter<PartMember, RecyclerView.ViewHolder>(diffUtil) {
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) HEADER
+        else CONTENT
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoAndroidViewHolder {
-        val binding =
-            ItemGoAndroidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return GoAndroidViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == HEADER) {
+            val binding = ItemGoAndroidHeaderBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            HeaderViewHolder(binding)
+        } else {
+            val binding =
+                ItemGoAndroidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            GoAndroidViewHolder(binding)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (position == HEADER)
+            (holder as HeaderViewHolder).onBind(currentList[position])
+        else
+            (holder as GoAndroidViewHolder).onBind(currentList[position])
+    }
+
+    class HeaderViewHolder(private val binding: ItemGoAndroidHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(header: PartMember) {
+            with(binding) {
+                ivGoAndroid.setImageResource(header.imageResourceContent)
+                tvGoAndroidHeaderTitle.text = header.stringContent
+            }
+        }
     }
 
     class GoAndroidViewHolder(private val binding: ItemGoAndroidBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(partMember: PartMember) {
             with(binding) {
-                ivGoAndroid.setImageResource(partMember.memberImage)
-                tvGoAndroidName.text = partMember.memberName
+                ivGoAndroid.setImageResource(partMember.imageResourceContent)
+                tvGoAndroidName.text = partMember.stringContent
             }
         }
-    }
-
-    override fun onBindViewHolder(holder: GoAndroidViewHolder, position: Int) {
-        holder.onBind(currentList[position])
     }
 
     companion object {
@@ -40,5 +67,8 @@ class GoAndroidAdapter : ListAdapter<PartMember, GoAndroidAdapter.GoAndroidViewH
                 oldItem == newItem
 
         }
+
+        private const val HEADER = 0
+        private const val CONTENT = 1
     }
 }
