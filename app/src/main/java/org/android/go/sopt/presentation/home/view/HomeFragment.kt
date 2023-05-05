5376407dd3c98123b612a4615f605c24c7dbcb74
giovannijunseokim <kingjunseo@gmail.com>
2023-05-05 19:26:30 +0900

    [chore] home 에 있는 요소들 gallery 로 이전4

--- app/src/main/java/org/android/go/sopt/presentation/home/view/HomeFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/view/HomeFragment.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.android.go.sopt.databinding.FragmentGalleryBinding
+import org.android.go.sopt.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
-    private val binding: FragmentGalleryBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { "binding is null ...." }
 
     override fun onCreateView(
@@ -18,7 +19,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
