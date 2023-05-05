54a92879cdf1b6a252d25c2054e1d7633f05d538
giovannijunseokim <kingjunseo@gmail.com>
2023-05-05 19:09:32 +0900

    [chore] home 에 있는 요소들 gallery 로 이전3

---
+++ app/src/main/java/org/android/go/sopt/presentation/gallery/view/GalleryFragment.kt
@@ -0,0 +1,64 @@
+package org.android.go.sopt.presentation.home.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
+import org.android.go.sopt.R
+import org.android.go.sopt.databinding.FragmentGalleryBinding
+import org.android.go.sopt.presentation.home.adapter.GoAndroidAdapter
+import org.android.go.sopt.presentation.home.adapter.selection.GoAndroidItemDetailsLookup
+import org.android.go.sopt.presentation.home.adapter.selection.SelectionItemKeyProvider
+import org.android.go.sopt.presentation.home.viewmodel.GalleryViewModel
+
+class GalleryFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding: FragmentGalleryBinding
+        get() = requireNotNull(_binding) { "binding is null ...." }
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        val adapter = GoAndroidAdapter()
+        binding.rvGallery.adapter = adapter
+        adapter.submitList(viewModel.getPartMemberList())
+        val tracker = buildTracker()
+        adapter.setSelectionTracker(tracker)
+    }
+
+    private fun buildTracker(): SelectionTracker<Long> = SelectionTracker.Builder(
+        getString(R.string.selected_member),
+        binding.rvGallery,
+        SelectionItemKeyProvider(binding.rvGallery),
+        GoAndroidItemDetailsLookup(binding.rvGallery),
+        StorageStrategy.createLongStorage(),
+    ).build()
+
+    fun smoothScrollToTop() {
+        binding.rvGallery.smoothScrollToPosition(0)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
