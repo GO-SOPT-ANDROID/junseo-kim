d84aa9f762bfbbdb7004b6fa866244d0b101934b
giovannijunseokim <kingjunseo@gmail.com>
2023-05-08 18:05:05 +0900

    seminar 4 amend

--- app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
@@ -9,7 +9,7 @@ import retrofit2.Retrofit
 object ApiFactory {
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://52.78.152.187:8080/")
+            .baseUrl("BASE_URL")
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
