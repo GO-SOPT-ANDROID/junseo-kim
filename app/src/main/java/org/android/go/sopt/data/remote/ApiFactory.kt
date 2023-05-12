199fc77638da5cc60bf8bdad9319969d203f0d95
giovannijunseokim <kingjunseo@gmail.com>
2023-05-12 22:27:07 +0900

    [feat] local properties 사용

--- app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
@@ -3,13 +3,14 @@ package org.android.go.sopt.data.remote
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import org.android.go.sopt.BuildConfig
 import org.android.go.sopt.data.remote.service.SignUpService
 import retrofit2.Retrofit
 
 object ApiFactory {
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("BASE_URL")
+            .baseUrl(BuildConfig.AUTH_BASE_URL)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
