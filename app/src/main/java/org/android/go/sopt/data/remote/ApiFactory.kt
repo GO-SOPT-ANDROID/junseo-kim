624e0314cb28bd988c6ae7f48a312ecbdcebd17e
giovannijunseokim <kingjunseo@gmail.com>
2023-05-13 00:42:33 +0900

    [feat] ServicePool에 ReqresService 추가

--- app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
@@ -3,23 +3,39 @@ package org.android.go.sopt.data.remote
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.android.go.sopt.BuildConfig
+import org.android.go.sopt.data.remote.services.ReqresService
 import org.android.go.sopt.data.remote.services.SignInService
 import org.android.go.sopt.data.remote.services.SignUpService
 import retrofit2.Retrofit
 
 object ApiFactory {
-    val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BuildConfig.AUTH_BASE_URL)
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-            .build()
+    private val client by lazy {
+        OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }).build()
     }
 
-    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+    val retrofitForAuth: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(BuildConfig.AUTH_BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType())).build()
+    }
+
+    inline fun <reified T> createAuthService(): T = retrofitForAuth.create<T>(T::class.java)
+
+    val retrofitForReqres: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(BuildConfig.REQRES_BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(client).build()
+    }
+
+    inline fun <reified T> createReqresService(): T = retrofitForReqres.create<T>(T::class.java)
 }
 
 object ServicePool {
-    val signUpService = ApiFactory.create<SignUpService>()
-    val signInService = ApiFactory.create<SignInService>()
+    val signUpService = ApiFactory.createAuthService<SignUpService>()
+    val signInService = ApiFactory.createAuthService<SignInService>()
+    val reqresService = ApiFactory.createReqresService<ReqresService>()
 }
