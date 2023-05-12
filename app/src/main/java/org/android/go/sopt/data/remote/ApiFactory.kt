759da9a9b9e4b480f03825301f2b6d0ed1d64a1e
giovannijunseokim <kingjunseo@gmail.com>
2023-05-12 23:15:22 +0900

    [feat] 로그인 기능 구현

--- app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
@@ -4,6 +4,7 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import org.android.go.sopt.BuildConfig
+import org.android.go.sopt.data.remote.services.SignInService
 import org.android.go.sopt.data.remote.services.SignUpService
 import retrofit2.Retrofit
 
@@ -20,4 +21,5 @@ object ApiFactory {
 
 object ServicePool {
     val signUpService = ApiFactory.create<SignUpService>()
+    val signInService = ApiFactory.create<SignInService>()
 }
--- app/src/main/java/org/android/go/sopt/presentation/signin/viewmodel/SignInViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signin/viewmodel/SignInViewModel.kt
@@ -1,28 +1,7 @@
 package org.android.go.sopt.presentation.signin.viewmodel
 
 import androidx.lifecycle.ViewModel
-import org.android.go.sopt.data.local.model.UserInfo
 
 class SignInViewModel : ViewModel() {
-    private lateinit var userInfo: UserInfo
-
-    fun getUserInfo(_userInfo: UserInfo) {
-        userInfo = _userInfo
-    }
-
-    fun isUserInfoCreated(): Boolean {
-        return ::userInfo.isInitialized
-    }
-
-    fun isUserInfoCorrect(inputId: String, inputPW: String): Boolean {
-        return userInfo.userId == inputId && userInfo.userPw == inputPW
-    }
-
-    fun getUserName(): String? {
-        return userInfo.userName
-    }
-
-    fun getUserSkill(): String? {
-        return userInfo.userSkill
-    }
+    private var name: String? = null
 }
