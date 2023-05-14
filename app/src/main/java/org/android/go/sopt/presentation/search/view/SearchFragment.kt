9ac4d0619d4b736a3b295b5b53e4e3044d3bb3a3
giovannijunseokim <kingjunseo@gmail.com>
2023-05-14 17:14:48 +0900

    [feat] Kakao Search 기능 구현 완료

--- app/src/main/java/org/android/go/sopt/presentation/search/view/SearchFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/search/view/SearchFragment.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import org.android.go.sopt.data.remote.ServicePool
+import org.android.go.sopt.data.remote.model.ResponseKakaoSearchDto
 import org.android.go.sopt.databinding.FragmentSearchBinding
+import org.android.go.sopt.presentation.search.adapter.KakaoSearchResultAdapter
+import org.android.go.sopt.util.PublicString.CONNECTION_FAIL
+import org.android.go.sopt.util.PublicString.UNEXPECTED_ERROR
+import org.android.go.sopt.util.extensions.makeToastMessage
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding: FragmentSearchBinding
         get() = requireNotNull(_binding) { "binding is null ...." }
+    private val kakaoSearchService by lazy { ServicePool.kakaoSearchService }
+    private val adapter by lazy { KakaoSearchResultAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +37,40 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.rvSearch.adapter = adapter
+
+        binding.svSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                kakaoSearchService.search(keyword = binding.svSearch.query.toString()).enqueue(
+                    object : Callback<ResponseKakaoSearchDto> {
+                        override fun onResponse(
+                            call: Call<ResponseKakaoSearchDto>,
+                            response: Response<ResponseKakaoSearchDto>,
+                        ) {
+                            if (response.isSuccessful) {
+                                adapter.submitList(
+                                    response.body()?.documents
+                                        ?: listOf<ResponseKakaoSearchDto.Document>()
+                                )
+                            } else {
+                                makeToastMessage(UNEXPECTED_ERROR)
+                            }
+                        }
+
+                        override fun onFailure(call: Call<ResponseKakaoSearchDto>, t: Throwable) {
+                            makeToastMessage(CONNECTION_FAIL)
+                        }
+
+                    }
+                )
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+
+        })
     }
 
     override fun onDestroyView() {
