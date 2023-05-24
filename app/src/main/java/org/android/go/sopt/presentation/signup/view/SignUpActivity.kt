6fb181b535fdd555653eb3163ff3350c963e6f79
giovannijunseokim <kingjunseo@gmail.com>
2023-05-24 20:53:37 +0900

    [feat] SignUpActivity Data Binding 적용하여 에러 메시지 반환

--- app/src/main/java/org/android/go/sopt/presentation/signup/view/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/view/SignUpActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.MotionEvent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import org.android.go.sopt.R
 import org.android.go.sopt.data.remote.model.RequestSignUpDto
 import org.android.go.sopt.databinding.ActivitySignUpBinding
@@ -15,12 +16,64 @@ import org.android.go.sopt.util.extensions.makeToastMessage
 
 class SignUpActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivitySignUpBinding.inflate(layoutInflater) }
+    lateinit var binding: ActivitySignUpBinding
     private val viewModel by viewModels<SignUpViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
 
+        initDataBinding()
+        setSignUpBtnClickEvent()
+        observeSignUpResult()
+        observeErrorResponse()
+
+        observeSignUpInfo()
+    }
+
+    private fun observeSignUpInfo() {
+        viewModel.isIdValid.observe(this) { isIdValid ->
+            if (isIdValid)
+                binding.layoutSignUpId.error = null
+            else
+                binding.layoutSignUpId.error = "조건 : 영문, 숫자가 포함된 6~10글자"
+        }
+
+        viewModel.isPwValid.observe(this) { isPwValid ->
+            if (isPwValid)
+                binding.layoutSignUpPw.error = null
+            else
+                binding.layoutSignUpPw.error = "조건 : 영문, 숫자, 특수문자가 포함된 6~12글자"
+        }
+    }
+
+    private fun initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+    }
+
+    private fun observeErrorResponse() {
+        viewModel.errorResult.observe(this) { errorMessage ->
+            makeToastMessage(
+                errorMessage
+            )
+        }
+    }
+
+    private fun observeSignUpResult() {
+        viewModel.signUpResult.observe(this) { signUpResult ->
+            startActivity(
+                Intent(
+                    this@SignUpActivity,
+                    SignInActivity::class.java
+                )
+            )
+            makeToastMessage(
+                signUpResult.message
+            )
+        }
+    }
+
+    private fun setSignUpBtnClickEvent() {
         with(binding) {
             btnSignUp.setOnClickListener {
                 if (canUserSignUp()) {
@@ -37,29 +90,11 @@ class SignUpActivity : AppCompatActivity() {
                 }
             }
         }
-
-        viewModel.signUpResult.observe(this) { signUpResult ->
-            startActivity(
-                Intent(
-                    this@SignUpActivity,
-                    SignInActivity::class.java
-                )
-            )
-            makeToastMessage(
-                signUpResult.message
-            )
-        }
-        viewModel.errorResult.observe(this) { errorMessage ->
-            makeToastMessage(
-                errorMessage
-            )
-        }
-
     }
 
     private fun canUserSignUp(): Boolean {
-        return binding.etSignUpId.text.length in 6..10 &&
-                binding.etSignUpPw.text.length in 8..12 &&
+        return (binding.etSignUpId.text?.length ?: 0) in (6..10) &&
+                (binding.etSignUpPw.text?.length ?: 0) in (8..12) &&
                 binding.etSignUpName.text.isNotBlank() &&
                 binding.etSignUpSkill.text.isNotBlank()
     }
--- app/src/main/java/org/android/go/sopt/presentation/signup/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/viewmodel/SignUpViewModel.kt
@@ -3,6 +3,7 @@ package org.android.go.sopt.presentation.signup.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import org.android.go.sopt.data.remote.ServicePool
 import org.android.go.sopt.data.remote.model.RequestSignUpDto
 import org.android.go.sopt.data.remote.model.ResponseSignUpDto
@@ -10,7 +11,28 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class SignUpViewModel : ViewModel() {
+
+    val id: MutableLiveData<String> = MutableLiveData("")
+    val isIdValid: LiveData<Boolean> = id.map { id ->
+        checkIdIsValid(id)
+    }
+
+    private fun checkIdIsValid(id: String): Boolean {
+        return id.isBlank() || id.length in 6..10
+    }
+
+    val pw: MutableLiveData<String> = MutableLiveData("")
+    val isPwValid: LiveData<Boolean> = pw.map { pw ->
+        checkPwIsValid(pw)
+    }
+
+    private fun checkPwIsValid(pw: String): Boolean {
+        return pw.isBlank() || pw.length in 6..10
+    }
+
+
     private val _signUpResult: MutableLiveData<ResponseSignUpDto> = MutableLiveData()
     val signUpResult: LiveData<ResponseSignUpDto> = _signUpResult
 
@@ -20,6 +42,7 @@ class SignUpViewModel : ViewModel() {
     fun signUp(userData: RequestSignUpDto) {
         ServicePool.signUpService.signUp(userData).enqueue(
             object : Callback<ResponseSignUpDto> {
+
                 override fun onResponse(
                     call: Call<ResponseSignUpDto>,
                     response: Response<ResponseSignUpDto>,
